2eebd427581e6f086bceb65424baee80bd5fa42f
Usmonaliyev Salohiddin <108422142+salohiddinusmonaliyev@users.noreply.github.com>
2023-08-16 14:47:53 +0000

    update

---
+++ staticfiles/chart.js
@@ -0,0 +1,11 @@
+const labels = Utils.months({count: 7});
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
+};
