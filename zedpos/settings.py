72c86b379a773072f2c1a3126299c548532fa4fd
Usmonaliyev Salohiddin <108422142+salohiddinusmonaliyev@users.noreply.github.com>
2024-01-05 22:28:55 +0500

    updated(v0.1)

--- zedpos/settings.py
+++ zedpos/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sf+s857!6!c5!quv+no21t0a+ujj95$y(ezmc&f8!!u(g36*zx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
